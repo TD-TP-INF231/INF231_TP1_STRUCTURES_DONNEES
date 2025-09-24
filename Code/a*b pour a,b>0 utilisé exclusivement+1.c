bdedb255d1be44eb6cfe92d962a0fafc5ae6324b
botagnejulien237-uy1 <julienclaudedanielbotagne@gmail.com>
2025-09-24 22:42:18 +0100

    Update a*b pour a,b>0 utilisé exclusivement+1.cCe code est utilisé car il réduit le nombre d’itérations tout en respectant la contrainte d’addition +1, rendant le calcul plus rapide que le code précédent.

--- Code/a*b pour a,b>0 utilisé exclusivement+1.c
+++ Code/a*b pour a,b>0 utilisé exclusivement+1.c
@@ -2,9 +2,18 @@
 
 int multiplication(int a, int b) {
     int result = 0;
-    for (int i = 0; i < b; i++) // Répète b fois
-        for (int j = 0; j < a; j++) // Ajoute 1, a fois
-            result += 1;
+    
+    // On choisit de répéter l'addition le moins de fois possible
+    int min = a < b ? a : b;
+    int max = a > b ? a : b;
+    
+    // Ajouter `max` à `result`, `min` fois
+    for (int i = 0; i < min; i++) {
+        for (int j = 0; j < max; j++) {
+            result += 1; // utilisation stricte de +1
+        }
+    }
+    
     return result;
 }
 
@@ -12,12 +21,12 @@ int main() {
     int a, b;
     printf("Entrez deux nombres positifs a et b : ");
     scanf("%d %d", &a, &b);
-    
+
     if (a <= 0 || b <= 0) {
         printf("Les nombres doivent etre positifs.\n");
         return 1;
     }
-    
+
     printf("%d x %d = %d\n", a, b, multiplication(a, b));
     return 0;
 }
