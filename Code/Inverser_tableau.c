14b8b493d04863b05ae0cd3b356f7a5ddc7714f0
botagnejulien237-uy1 <julienclaudedanielbotagne@gmail.com>
2025-09-24 16:30:18 +0100

    Update Inverser_tableau.c

--- Code/Inverser_tableau.c
+++ Code/Inverser_tableau.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Fonction pour inverser un tableau
 void inverser_tableau(int tab[], int taille) {
     int temp;
     for (int i = 0; i < taille / 2; i++) {
@@ -10,8 +11,18 @@ void inverser_tableau(int tab[], int taille) {
 }
 
 int main() {
-    int tab[] = {1, 2, 3, 4, 5};
-    int taille = sizeof(tab) / sizeof(tab[0]);
+    int taille;
+
+    printf("Entrez la taille du tableau : ");
+    scanf("%d", &taille);
+
+    int tab[taille];
+
+    // Saisie des éléments du tableau
+    for (int i = 0; i < taille; i++) {
+        printf("Entrez l'élément %d : ", i + 1);
+        scanf("%d", &tab[i]);
+    }
 
     printf("Tableau original : ");
     for (int i = 0; i < taille; i++)
